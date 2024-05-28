c0fcec5c905da06296eaea47c8d6b16c2dc58fc8
Cybernetic Systems <39177227+dJOS1475@users.noreply.github.com>
2024-05-28 13:28:17 +1000

    Add files via upload

--- Fronius_Solar_Inverter.groovy
+++ Fronius_Solar_Inverter.groovy
@@ -12,7 +12,7 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
- *  May 2024 - Modified by Derek Osborn 
+ *  May 2024 - Forked by Derek Osborn 
  *	v2.0.0 - Updated to convert Watt hours to Kilowatt hours, Added Dashboard tile, and published to HPM for easier access
  *  
  *  Source:
